f472afd4e44caadb24b0e0d60202fe55f27d855e
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-02-14 11:00:34 +0100

    feat: add bd

---
+++ Context/GamelibContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace gamelib.Context;
+
+public class GamelibContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=gamelib.db");
+        optionsBuilder.UseLazyLoadingProxies();
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,21 @@
-﻿namespace gamelib;
+﻿using gamelib.Context;
+
+namespace gamelib;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow
 {
+    private readonly GamelibContext _context;
+
     public MainWindow()
     {
+        _context = new GamelibContext();
+        _context.Database.EnsureCreated();
+
+        Closed += (_, _) => _context.Dispose();
+
         InitializeComponent();
     }
 }
