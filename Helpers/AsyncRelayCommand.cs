f1bb83d2583e9f5bfcbf3c1fb7fe0445704a5fe6
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-02-14 09:46:02 +0100

    feat: add command helpers

---
+++ Helpers/AsyncRelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Input;
+
+namespace gamelib.Helpers;
+
+public class AsyncRelayCommand(
+    Func<Task> execute,
+    Func<bool>? canExecute = null
+) : ICommand
+{
+    private readonly Func<Task> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+    private bool _isExecuting;
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return !_isExecuting && (canExecute?.Invoke() ?? true);
+    }
+
+    public async void Execute(object? parameter)
+    {
+        await ExecuteAsync(parameter);
+    }
+
+    private async Task ExecuteAsync(object? parameter)
+    {
+        if (CanExecute(parameter))
+        {
+            try
+            {
+                _isExecuting = true;
+                RaiseCanExecuteChanged();
+                await _execute();
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+    }
+
+    private void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ Helpers/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+
+namespace gamelib.Helpers;
+
+public class RelayCommand(
+    Action<object?> execute,
+    Predicate<object?>? canExecute = null
+) : ICommand
+{
+    private readonly Action<object?> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+
+    public bool CanExecute(object? parameter)
+    {
+        return canExecute == null || canExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+}
