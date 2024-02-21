5ba488ed3d2689db1c46d00d9443b3c58f2f1b61
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-02-21 10:40:17 +0100

    feat: catch exception

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Threading;
 using gamelib.Context;
+using gamelib.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace gamelib;
@@ -15,9 +15,48 @@ public partial class App : Application
     {
         base.OnStartup(e);
 
-        using (var context = new GamelibContext())
+        try
         {
-            context.Database.Migrate();
+            using (var context = new GamelibContext())
+            {
+                context.Database.Migrate();
+            }
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+
+        DispatcherUnhandledException += Application_DispatcherUnhandledException;
+    }
+
+    private void Application_DispatcherUnhandledException(object sender,
+        DispatcherUnhandledExceptionEventArgs e)
+    {
+        e.Handled = true;
+        HandleException(e.Exception);
+    }
+
+    private void HandleException(Exception exception)
+    {
+        if (exception is GamelibException gamelibException)
+        {
+            MessageBox.Show(
+                $"{exception.Message}",
+                gamelibException.Caption,
+                MessageBoxButton.OK
+            );
+        }
+        else
+        {
+            MessageBox.Show(
+                $"Une erreur vient de se produire : {exception.Message}",
+                "Erreur",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error
+            );
+
+            Current.Shutdown();
         }
     }
 }
