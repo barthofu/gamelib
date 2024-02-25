0ba9b879acfe8147cc0686616cf24ae355c33e4a
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-02-25 11:53:22 +0100

    feat: remove proxy

--- ViewModels/Pages/HomeViewModel.cs
+++ ViewModels/Pages/HomeViewModel.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Windows.Controls;
 using System.Windows.Input;
+using gamelib.Helpers;
 using gamelib.Models;
 using gamelib.Services;
 using gamelib.Views.Pages;
 using gamelib.Views.Windows;
-using Wpf.Ui.Mvvm.Contracts;
 
 namespace gamelib.ViewModels.Pages;
 
@@ -14,20 +13,22 @@ public class HomeViewModel : INotifyPropertyChanged
 {
     private readonly GameService _gameService;
     private readonly MainWindow _mainWindow;
-    
+
     private string _filterQuery = string.Empty;
-    
+
     public ObservableCollection<Game> FilteredGames { get; } = [];
-    
+
+    public ICommand OnGameItemClickCommand => new RelayCommand(OnGameItemClick);
+
     public HomeViewModel(GameService gameService, MainWindow mainWindow)
     {
         _gameService = gameService;
         _mainWindow = mainWindow;
-        
+
         // init game list
         SearchGames();
     }
-    
+
     public string FilterQuery
     {
         get => _filterQuery;
@@ -51,15 +52,14 @@ public class HomeViewModel : INotifyPropertyChanged
         SearchGames();
     }
 
-    public void OnGameItemClick(object sender, MouseButtonEventArgs e)
+    private void OnGameItemClick(object? sender)
     {
-        if (sender is not Border border) return;
-        if (border.DataContext is not Game game) return;
-        
+        if (sender is not Game game) return;
+
         var gameDetailsPage = new GameDetailsPage(game);
         _mainWindow.MainFrame.NavigationService?.Navigate(gameDetailsPage);
     }
-    
+
     public event PropertyChangedEventHandler? PropertyChanged;
 
     private void OnPropertyChanged(string propertyName)
