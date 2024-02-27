858804ace05d50125d7ddbc9dc4f3737173a0165
Bartholomé Gili <dev.bartho@gmail.com>
2024-02-27 13:43:26 +0100

    refacto: move all source files to `src/`

---
+++ src/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gamelib.Models;
+
+public class Genre
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    // relations
+    public virtual ICollection<Game> Games { get; } = new List<Game>();
+}
